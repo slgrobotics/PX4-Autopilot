a270845a932c159ab475196cf83f3e3c53c0bac2
Sergei <msghub@hotmail.com>
2024-02-25 11:59:30 -0600

    copy script to set world correctly

---
+++ Tools_gazebo/copy_to_Tools_gz.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export SITL_GAZEBO=../Tools/simulation/gz
+
+set -x
+cp default.sdf $SITL_GAZEBO/worlds/default.sdf
+#cp $SITL_GAZEBO/worlds/default.sdf .
+
+ls -al $SITL_GAZEBO/worlds/.
+ls -al
+set +x
+
