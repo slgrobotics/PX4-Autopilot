0c4aa1a898db6092ae55fe805a9f359b62c0ba87
Sergei Grichine <msghub@hotmail.com>
2025-06-18 20:30:06 -0500

    Updated lawnmower SIM parameters and started rover_pos_control

---
+++ make.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -x
+
+#make check_format
+#make format
+
+make px4_sitl gz_lawnmower_lawn
+#make px4_sitl gz_lawnmower
+#make px4_sitl gz_r1_rover
+
+#make emlid_navio2_arm64
+#make emlid_navio2_arm64 upload
+
+#make scumaker_pilotpi
+#make scumaker_pilotpi_arm64
+#make px4_raspberrypi
+#make px4_raspberrypi_arm64
+
+set +x
